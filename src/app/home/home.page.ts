e53d2b9ad502d5d83a931bd8983e74cd2d4f3335
YhonVivas <yohnvivas16@gmail.com>
2024-11-10 21:36:12 -0300

    Preferences ready, styles changed

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,5 +1,7 @@
 import { Component, ViewChild } from '@angular/core';
-import { IonInput } from '@ionic/angular';
+import { IonInput, ActionSheetController } from '@ionic/angular';
+import { Preferences } from '@capacitor/preferences';
+import { taskModel } from '../model/task-mode';
 
 @Component({
   selector: 'app-home',
@@ -7,20 +9,66 @@ import { IonInput } from '@ionic/angular';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
-
   @ViewChild('taskInput', { static: false }) taskInput!: IonInput;
-  tasks: { name: string, status: boolean }[] = [];
+  tasks: taskModel[] = [];
   segment: string = 'Doing';
+  isInputValid: boolean = false;
 
-  constructor() {}
+
+  constructor(private actionSheetController: ActionSheetController) {}
+
+  async ionViewWillEnter() {
+    const task = await Preferences.get({ key: 'tasks' });
+    if (!task.value) {
+      Preferences.set({ key: 'tasks', value: JSON.stringify(this.tasks) });
+    } else {
+      this.tasks = JSON.parse(task.value);
+    }
+  }
 
   addNewTask(taskName: string) {
     if (taskName && taskName.trim() !== '') {
-      this.tasks.push({ name: taskName, status: false });
+      this.tasks.push({ name: taskName.trim(), status: false });
+      this.saveTask();
       this.taskInput.value = '';
-    }}
+    }
+  }
 
   deleteTask(index: number) {
     this.tasks.splice(index, 1);
+    this.saveTask();
   }
+
+  changeStatus(index: number) {
+    if (this.tasks[index].status == false) {
+      this.tasks[index].status = true;
+      this.saveTask();
+    }
+  }
+
+  saveTask() {
+    console.log('Guardando una nueva tarea...');
+    Preferences.set({ key: 'tasks', value: JSON.stringify(this.tasks) });
+  }
+
+  async presentActionSheet(index: number) {
+    const actionSheet = await this.actionSheetController.create({
+      header: 'Actions',
+      buttons: [
+        {
+          text: 'Delete',
+          role: 'destructive',
+          handler: () => {
+            this.deleteTask(index);
+          },
+        },
+        {
+          text: 'Cancel',
+          role: 'cancel',
+        },
+      ],
+    });
+    await actionSheet.present();
+  }
+
 }
---
+++ src/app/model/task-mode.ts
@@ -0,0 +1,7 @@
+export interface taskModel{
+    name: string;
+    status: boolean;
+    day?: string;
+    month?: string;
+    year?: string;
+}[]
